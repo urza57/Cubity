77e6fc07d14febdcabba62fed590128f12b77e84
Arnaud Colin <colin.arnaud9@gmail.com>
2015-01-24 11:44:18 +0100

    add scripts

---
+++ Assets/Scripts/obtainedKey.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class obtainedKey : MonoBehaviour {
+
+	public GameObject interrupteur;
+	public GameObject player;
+	public float distanceX;
+	public float distanceMin = 0.7f;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("First Person Controller");
+		interrupteur.renderer.material.color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xP = player.transform.position.x;
+		float xC = gameObject.transform.position.x;
+
+		if(xP >= xC)
+			distanceX = xP - xC;
+		else
+			distanceX = xC - xP;
+		if(distanceX <= distanceMin){
+			interrupteur.renderer.material.color = Color.green;
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/upAndDownKey.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class upAndDownKey : MonoBehaviour {
+
+	public float moveSpeed = 2.5f;
+	public bool isUp = true;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameObject.transform.position.y <= 10.0f && isUp == true){
+			transform.Translate(new Vector3 (0, 1, 0) * Time.deltaTime * moveSpeed);
+		}
+		else {
+			isUp = false;
+			if(gameObject.transform.position.y >= 2.0f)
+				transform.Translate(new Vector3 (0, -1, 0) * Time.deltaTime * moveSpeed);
+			else
+				isUp = true;
+		}
+	}
+}
