66a4d575c39cda752c25549f03a7304eed4a8dab
adipino <alexandre.dipino@insimo.fr>
2015-01-25 13:44:12 +0100

    Update : add final room

---
+++ Assets/Scripts/crazyCube3.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class crazyCube3 : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.right, Time.deltaTime * 100);
+		transform.Rotate(Vector3.up, Time.deltaTime * 100, Space.World);
+	}
+}
---
+++ Assets/Scripts/teleport.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class teleport : MonoBehaviour {
+
+	public GameObject tp;
+	private GameObject player;
+
+	void Start()
+	{
+		player = GameObject.Find("First Person Controller");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance(this.transform.position, player.transform.position) < 2.0f)
+		{
+			player.transform.position = tp.transform.position;
+		}
+	}
+}
