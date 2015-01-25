f88f3e24bc7b874b2605d15dd00f410e9fa633c3
Arnaud Colin <colin.arnaud9@gmail.com>
2015-01-25 13:45:42 +0100

    add stuff
    add stuff

---
+++ Assets/Scripts/loadLevelAfterImage.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadLevelAfterImage : MonoBehaviour {
+	
+	public bool isEnd = false;
+	public float time;
+
+	// Use this for initialization
+	void Start () {
+		time = 5.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		isEnd = false;
+
+		if(time >= 0.0f){
+			time -= Time.deltaTime;
+		}
+		else{
+			StartTheGame();
+		}
+	}	
+
+	public void ExitTheGame()
+	{
+		Application.Quit();
+	}
+	
+	public void StartTheGame()
+	{
+
+		Application.LoadLevel("test");
+	}
+}
