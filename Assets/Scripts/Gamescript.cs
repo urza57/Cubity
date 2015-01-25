c48c118e509bd3c06f79ec1e0a640539a09e0bbf
Arnaud Colin <colin.arnaud9@gmail.com>
2015-01-25 10:30:00 +0100

    add some stuff

--- Assets/Scripts/Gamescript.cs
+++ Assets/Scripts/Gamescript.cs
@@ -3,18 +3,29 @@ using System.Collections;
 
 public class Gamescript : MonoBehaviour {
 
+	public Texture2D cursorImage;
+	private int cursorWidth = 64;
+	private int cursorHeight = 64;
+
+	/*public Texture2D cursorTexture;
+	public CursorMode cursorMode = CursorMode.Auto;
+	public Vector2 hotSpot = Vector2.zero;*/
+
 	// Use this for initialization
 	void Start () {
-	
+		Screen.showCursor = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Screen.lockCursor = true;
 		if (Input.GetKey(KeyCode.Escape))
 		{
 			Application.Quit();
 		}
-		
+	}
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect(Input.mousePosition.x, Screen.height - Input.mousePosition.y, cursorWidth, cursorHeight), cursorImage);
+		Screen.lockCursor = true;
 	}
 }
